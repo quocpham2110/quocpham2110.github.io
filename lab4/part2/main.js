2fcc73b7768d502fb305ec341d52c35d2245f896
quocpham <quocpham2110@gmail.com>
2024-02-20 09:49:21 -0500

    declare the image name list, and image alternative text list

--- lab4/part2/main.js
+++ lab4/part2/main.js
@@ -5,9 +5,22 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
+const imageNameList = [
+  'pic1.jpg',
+  'pic2.jpg',
+  'pic3.jpg',
+  'pic4.jpg',
+  'pic5.jpg',
+];
 
 /* Declaring the alternative text for each image file */
-
+const imageAltList = [
+  'Closeup of a human eye',
+  'Stone patterns in nature',
+  'Purple flowers',
+  'Ancient Egypt',
+  'Butterfly lands on the leaf',
+];
 /* Looping through images */
 
 const newImage = document.createElement('img');
